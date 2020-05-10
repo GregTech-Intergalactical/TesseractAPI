5f571de9893fc6dd7c3e2075831f7c935a257e31
TakIsADog <haydenkilloh@gmail.com>
2020-05-10 15:54:19 +0100

    allow passing a custom controller when registering to the API

--- src/main/java/tesseract/TesseractAPI.java
+++ src/main/java/tesseract/TesseractAPI.java
@@ -2,6 +2,7 @@ package tesseract;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import tesseract.api.Controller;
 import tesseract.api.electric.ElectricController;
 import tesseract.api.electric.IElectricCable;
 import tesseract.api.electric.IElectricEvent;
@@ -13,7 +14,10 @@ import tesseract.api.fluid.IFluidPipe;
 import tesseract.api.item.IItemNode;
 import tesseract.api.item.IItemPipe;
 import tesseract.api.item.ItemController;
-import tesseract.graph.*;
+import tesseract.graph.Cache;
+import tesseract.graph.Graph;
+import tesseract.graph.Group;
+import tesseract.graph.ITickingController;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -69,7 +73,18 @@ public class TesseractAPI {
      * @param node The node object.
      */
     public static void registerElectricNode(int dim, long pos, IElectricNode node) {
-        getElectricGraph(dim).addNode(pos, new Cache<>(node), new ElectricController(dim));
+        registerElectricNode(dim, pos, node, new ElectricController(dim));
+    }
+
+    /**
+     * Creates an instance of a class for a given node.
+     * @param dim The dimension id where the node will be added.
+     * @param pos The position at which the node will be added.
+     * @param node The node object.
+     * @param controller The controller for the node
+     */
+    public static void registerElectricNode(int dim, long pos, IElectricNode node, Controller<IElectricCable, IElectricNode> controller) {
+        getElectricGraph(dim).addNode(pos, new Cache<>(node), controller);
     }
 
     /**
@@ -79,7 +94,18 @@ public class TesseractAPI {
      * @param node The node object.
      */
     public static void registerFluidNode(int dim, long pos, IFluidNode node) {
-        getFluidGraph(dim).addNode(pos, new Cache<>(node), new FluidController(dim));
+        registerFluidNode(dim, pos, node, new FluidController(dim));
+    }
+
+    /**
+     * Creates an instance of a class for a given node.
+     * @param dim The dimension id where the node will be added.
+     * @param pos The position at which the node will be added.
+     * @param node The node object.
+     * @param controller The controller for the node
+     */
+    public static void registerFluidNode(int dim, long pos, IFluidNode node, Controller<IFluidPipe, IFluidNode> controller) {
+        getFluidGraph(dim).addNode(pos, new Cache<>(node), controller);
     }
 
     /**
@@ -89,7 +115,17 @@ public class TesseractAPI {
      * @param node The node object.
      */
     public static void registerItemNode(int dim, long pos, IItemNode node) {
-        getItemGraph(dim).addNode(pos, new Cache<>(node), new ItemController(dim));
+        registerItemNode(dim, pos, node, new ItemController(dim));
+    }
+
+    /**
+     * Creates an instance of a class for a given node.
+     * @param dim The dimension id where the node will be added.
+     * @param pos The position at which the node will be added.
+     * @param controller The controller for the node
+     */
+    public static void registerItemNode(int dim, long pos, IItemNode node, Controller<IItemPipe, IItemNode> controller) {
+        getItemGraph(dim).addNode(pos, new Cache<>(node), controller);
     }
 
     /**
@@ -99,7 +135,18 @@ public class TesseractAPI {
      * @param connector The cable object.
      */
     public static void registerElectricCable(int dim, long pos, IElectricCable connector) {
-        getElectricGraph(dim).addConnector(pos, new Cache<>(connector), new ElectricController(dim));
+        registerElectricCable(dim, pos, connector, new ElectricController(dim));
+    }
+
+    /**
+     * Creates an instance of a class for a given cable connector.
+     * @param dim The dimension id where the cable will be added.
+     * @param pos The position at which the cable will be added.
+     * @param connector The cable object.
+     * @param controller The controller for the connector
+     */
+    public static void registerElectricCable(int dim, long pos, IElectricCable connector, Controller<IElectricCable, IElectricNode> controller) {
+        getElectricGraph(dim).addConnector(pos, new Cache<>(connector), controller);
     }
 
     /**
@@ -109,7 +156,18 @@ public class TesseractAPI {
      * @param connector The pipe object.
      */
     public static void registerFluidPipe(int dim, long pos, IFluidPipe connector) {
-        getFluidGraph(dim).addConnector(pos, new Cache<>(connector), new FluidController(dim));
+        registerFluidPipe(dim, pos, connector, new FluidController(dim));
+    }
+
+    /**
+     * Creates an instance of a class for a given pipe connector.
+     * @param dim The dimension id where the pipe will be added.
+     * @param pos The position at which the pipe will be added.
+     * @param connector The pipe object.
+     * @param controller The controller for the connector
+     */
+    public static void registerFluidPipe(int dim, long pos, IFluidPipe connector, Controller<IFluidPipe, IFluidNode> controller) {
+        getFluidGraph(dim).addConnector(pos, new Cache<>(connector), controller);
     }
 
     /**
@@ -119,7 +177,18 @@ public class TesseractAPI {
      * @param connector The pipe object.
      */
     public static void registerItemPipe(int dim, long pos, IItemPipe connector) {
-        getItemGraph(dim).addConnector(pos, new Cache<>(connector), new ItemController(dim));
+        registerItemPipe(dim, pos, connector, new ItemController(dim));
+    }
+
+    /**
+     * Creates an instance of a class for a given pipe connector.
+     * @param dim The dimension id where the pipe will be added.
+     * @param pos The position at which the pipe will be added.
+     * @param connector The pipe object.
+     * @param controller The controller for the connector
+     */
+    public static void registerItemPipe(int dim, long pos, IItemPipe connector, Controller<IItemPipe, IItemNode> controller) {
+        getItemGraph(dim).addConnector(pos, new Cache<>(connector), controller);
     }
 
     /**
