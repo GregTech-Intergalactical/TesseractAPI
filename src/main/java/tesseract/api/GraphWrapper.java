86dbc7d06720b31ffd9ccd3f2567d3b7cac4ecd7
qubka <ushakovn@tcd.ie>
2020-05-14 16:04:51 +0100

    Remove RandomIterator + some refactor of wrapper

--- src/main/java/tesseract/api/GraphWrapper.java
+++ src/main/java/tesseract/api/GraphWrapper.java
@@ -4,71 +4,86 @@ import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import tesseract.graph.*;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.function.Function;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.function.IntFunction;
 
-public class GraphWrapper<P extends IConnectable, N extends IConnectable> {
+@ParametersAreNonnullByDefault
+public class GraphWrapper<C extends IConnectable, N extends IConnectable> {
 
-    protected Int2ObjectMap<Graph<P, N>> GRAPH = new Int2ObjectOpenHashMap<>();
-    protected Function<Integer, Controller<P, N>> defaultControllerSupplier;
+    protected final Int2ObjectMap<Graph<C, N>> graph = new Int2ObjectOpenHashMap<>();
+    protected final IntFunction<Controller<C, N>> supplier;
 
-    public GraphWrapper(Function<Integer, Controller<P, N>> defaultControllerSupplier) {
-        this.defaultControllerSupplier = defaultControllerSupplier;
+    /**
+     * Creates a graph wrapper.
+     *
+     * @param supplier The default controller supplier.
+     */
+    public GraphWrapper(IntFunction<Controller<C, N>> supplier) {
+        this.supplier = supplier;
     }
 
     /**
      * Creates an instance of a class for a given node.
+     *
      * @param dim The dimension id where the node will be added.
      * @param pos The position at which the node will be added.
      * @param node The node object.
      * @param controller The controller for the node
      */
-    public void registerNode(int dim, long pos, N node, Controller<P, N> controller) {
+    public void registerNode(int dim, long pos, N node, Controller<C, N> controller) {
         getGraph(dim).addNode(pos, new Cache<>(node), controller);
     }
 
     /**
      * Creates an instance of a class for a given node.
+     *
      * @param dim The dimension id where the node will be added.
      * @param pos The position at which the node will be added.
      * @param node The node object.
      */
     public void registerNode(int dim, long pos, N node) {
-        getGraph(dim).addNode(pos, new Cache<>(node), defaultControllerSupplier.apply(dim));
+        getGraph(dim).addNode(pos, new Cache<>(node), supplier.apply(dim));
     }
 
     /**
      * Creates an instance of a class for a given connector.
+     *
      * @param dim The dimension id where the node will be added.
      * @param pos The position at which the node will be added.
      * @param connector The connector object.
      * @param controller The controller for the node
      */
-    public void registerConnector(int dim, long pos, P connector, Controller<P, N> controller) {
+    public void registerConnector(int dim, long pos, C connector, Controller<C, N> controller) {
         getGraph(dim).addConnector(pos, new Cache<>(connector), controller);
     }
 
     /**
      * Creates an instance of a class for a given connector.
+     *
      * @param dim The dimension id where the node will be added.
      * @param pos The position at which the node will be added.
      * @param connector The connector object.
      */
-    public void registerConnector(int dim, long pos, P connector) {
-        getGraph(dim).addConnector(pos, new Cache<>(connector), defaultControllerSupplier.apply(dim));
+    public void registerConnector(int dim, long pos, C connector) {
+        getGraph(dim).addConnector(pos, new Cache<>(connector), supplier.apply(dim));
     }
 
     /**
      * Gets the graph for the type and dimension and will be instantiated if it does not already exist.
+     *
      * @param dim The dimension id.
      * @return The graph instance for the world.
      */
-    public Graph<P, N> getGraph(int dim) {
-        return GRAPH.computeIfAbsent(dim, k -> new Graph<>());
+    @Nonnull
+    public Graph<C, N> getGraph(int dim) {
+        return graph.computeIfAbsent(dim, k -> new Graph<>());
     }
 
     /**
      * Gets an instance of a controller at a given position.
+     *
      * @param dim The dimension id where the electric component is exist.
      * @param pos The position at which the electric component is exist.
      * @return The controller object. (Can be null)
@@ -81,6 +96,7 @@ public class GraphWrapper<P extends IConnectable, N extends IConnectable> {
 
     /**
      * Removes an instance of a class at the given position.
+     *
      * @param dim The dimension id where the electric component will be added.
      * @param pos The position at which the electric component will be added.
      */
