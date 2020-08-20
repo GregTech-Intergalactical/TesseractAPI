e13aeebb3d8fb043a0fc32e52d92eba1172e95e0
Axelandre42 <5228298+Axelandre42@users.noreply.github.com>
2020-08-21 01:56:44 +0200

    Move Tesseract.java (forge mod)

--- forge/src/main/java/tesseract/Tesseract.java
+++ forge/src/main/java/tesseract/Tesseract.java
@@ -15,14 +15,10 @@ import tesseract.api.item.ItemController;
 @Mod(Tesseract.API_ID)
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class Tesseract {
-
 	public static final String API_ID = "tesseract";
 	public static final String API_NAME = "Tesseract API";
 
 	public static final GraphWrapper<IElectricCable, IElectricNode> ELECTRIC = new GraphWrapper<>(ElectricController::new);
 	public static final GraphWrapper<IItemPipe, IItemNode> ITEM = new GraphWrapper<>(ItemController::new);
 	public static final GraphWrapper<IFluidPipe, IFluidNode> FLUID = new GraphWrapper<>(FluidController::new);
-
-	public Tesseract() {
-	}
 }
