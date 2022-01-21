c4695b0dd6b3e0a48b8659ca89f31f3d2ce99611
Abbe <xvlirox@gmail.com>
2022-01-21 09:55:06 +0100

    update modid to fix crashes (to tesseractAPI)

--- src/main/java/tesseract/Tesseract.java
+++ src/main/java/tesseract/Tesseract.java
@@ -33,7 +33,7 @@ import java.util.Set;
 //@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class Tesseract {
 
-    public static final String API_ID = "tesseract";
+    public static final String API_ID = "tesseractapi";
     public static final String API_NAME = "Tesseract API";
     public static final String VERSION = "0.0.1";
     public static final String DEPENDS = "";
