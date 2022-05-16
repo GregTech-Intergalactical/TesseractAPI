bc2aa3ac0dc8dbab28df7f5b5b6c6071e7ec9b12
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-05-15 22:16:08 -0400

    added NonNull Consumer, Supplier, and Function

--- common-dep/src/main/java/net/minecraftforge/common/util/LazyOptional.java
+++ common-dep/src/main/java/net/minecraftforge/common/util/LazyOptional.java
@@ -10,9 +10,6 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import io.github.fabricators_of_create.porting_lib.util.NonNullConsumer;
-import io.github.fabricators_of_create.porting_lib.util.NonNullFunction;
-import io.github.fabricators_of_create.porting_lib.util.NonNullSupplier;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import org.apache.commons.lang3.mutable.Mutable;
 import org.apache.commons.lang3.mutable.MutableObject;
