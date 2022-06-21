435600f0b083b00c8caad50ac25597c2c4689a13
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-06-20 23:10:06 -0400

    removed fabricated forge modules and replaced them with jitpack deps

--- fabricated-forge-recipe-api/src/main/java/net/minecraftforge/common/crafting/conditions/ICondition.java
+++
@@ -1,9 +0,0 @@
-package net.minecraftforge.common.crafting.conditions;
-
-import net.minecraft.resources.ResourceLocation;
-
-public interface ICondition {
-    ResourceLocation getID();
-
-    boolean test();
-}
