1c95c5f350bbbfde17f2fcfcb4607188463fd4c6
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-08-06 22:09:08 -0400

    proper fix for injection interface compile error

--- fabric/src/main/java/tesseract/fabric/FluidPlatformUtilsImpl.java
+++ fabric/src/main/java/tesseract/fabric/FluidPlatformUtilsImpl.java
@@ -1,6 +1,5 @@
 package tesseract.fabric;
 
-import io.github.fabricators_of_create.porting_lib.extensions.FluidExtensions;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
@@ -14,29 +13,29 @@ public class FluidPlatformUtilsImpl {
     }
 
     public static ResourceLocation getStillTexture(Fluid fluid){
-        return ((FluidExtensions)fluid).getAttributes().getStillTexture();
+        return fluid.getAttributes().getStillTexture();
     }
 
     public static ResourceLocation getFlowingTexture(Fluid fluid){
-        return ((FluidExtensions)fluid).getAttributes().getFlowingTexture();
+        return fluid.getAttributes().getFlowingTexture();
     }
     public static ResourceLocation getFluidId(Fluid fluid){
         return Registry.FLUID.getKey(fluid);
     }
 
     public static int getFluidTemperature(Fluid fluid){
-        return ((FluidExtensions)fluid).getAttributes().getTemperature();
+        return fluid.getAttributes().getTemperature();
     }
 
     public static boolean isFluidGaseous(Fluid fluid){
-        return ((FluidExtensions)fluid).getAttributes().isGaseous();
+        return fluid.getAttributes().isGaseous();
     }
 
     public static int getFluidColor(Fluid fluid){
-        return ((FluidExtensions)fluid).getAttributes().getColor();
+        return fluid.getAttributes().getColor();
     }
 
     public static SoundEvent getFluidSound(Fluid fluid, boolean fill){
-        return fill ? ((FluidExtensions)fluid).getAttributes().getFillSound() : ((FluidExtensions)fluid).getAttributes().getEmptySound();
+        return fill ? fluid.getAttributes().getFillSound() : fluid.getAttributes().getEmptySound();
     }
 }
