f53093cd7895412b76a0998ec2b887822dbc5ba6
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-04-06 17:09:25 -0400

    finished 1.19.2 port

--- fabric/src/main/java/tesseract/fabric/FluidPlatformUtilsImpl.java
+++ fabric/src/main/java/tesseract/fabric/FluidPlatformUtilsImpl.java
@@ -1,7 +1,12 @@
 package tesseract.fabric;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
+import net.fabricmc.fabric.api.transfer.v1.client.fluid.FluidVariantRendering;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariantAttributes;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -17,11 +22,23 @@ public class FluidPlatformUtilsImpl {
     }
 
     public static ResourceLocation getStillTexture(Fluid fluid){
-        return fluid.getAttributes().getStillTexture();
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            FluidVariant variant = FluidVariant.of(fluid);
+            TextureAtlasSprite[] sprites = FluidVariantRendering.getSprites(variant);
+
+            return sprites[0] == null ? SimpleFluidRenderHandler.WATER_STILL : sprites[0].getName();
+        }
+        return null;
     }
 
     public static ResourceLocation getFlowingTexture(Fluid fluid){
-        return fluid.getAttributes().getFlowingTexture();
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            FluidVariant variant = FluidVariant.of(fluid);
+            TextureAtlasSprite[] sprites = FluidVariantRendering.getSprites(variant);
+
+            return sprites[1] == null ? SimpleFluidRenderHandler.WATER_STILL : sprites[1].getName();
+        }
+        return null;
     }
     public static ResourceLocation getFluidId(Fluid fluid){
         return Registry.FLUID.getKey(fluid);
@@ -36,7 +53,10 @@ public class FluidPlatformUtilsImpl {
     }
 
     public static int getFluidColor(Fluid fluid){
-        return fluid.getAttributes().getColor();
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            return FluidVariantRendering.getColor(FluidVariant.of(fluid));
+        }
+        return -1;
     }
 
     public static SoundEvent getFluidSound(Fluid fluid, boolean fill){
