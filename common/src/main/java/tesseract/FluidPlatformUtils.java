0bf2cd19677bf5b47046d29f5a1916e2bb778396
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-07-20 01:04:26 -0400

    added fill and empty item methods

--- common/src/main/java/tesseract/FluidPlatformUtils.java
+++ common/src/main/java/tesseract/FluidPlatformUtils.java
@@ -1,8 +1,12 @@
 package tesseract;
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import earth.terrarium.botarium.common.fluid.base.FluidContainer;
 import earth.terrarium.botarium.common.fluid.base.FluidHolder;
+import earth.terrarium.botarium.common.fluid.base.PlatformFluidHandler;
+import earth.terrarium.botarium.common.fluid.base.PlatformFluidItemHandler;
 import earth.terrarium.botarium.common.fluid.utils.FluidHooks;
+import earth.terrarium.botarium.common.item.ItemStackHolder;
 import net.minecraft.core.Registry;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
@@ -65,6 +69,47 @@ public class FluidPlatformUtils {
     public static FluidStack tryFluidTransfer(IFluidHandler fluidDestination, IFluidHandler fluidSource, long maxAmount, boolean doTransfer){
         throw new AssertionError();
     }*/
+
+    public static long moveStandardToItemFluid(FluidContainer from, PlatformFluidItemHandler to, ItemStackHolder receiver, FluidHolder fluid) {
+        FluidHolder extracted = from.extractFluid(fluid, true);
+        long inserted = to.insertFluid(receiver.copy(), extracted, true);
+        from.extractFluid(FluidHooks.newFluidHolder(fluid.getFluid(), inserted, fluid.getCompound()), false);
+        return to.insertFluid(receiver, extracted, false);
+    }
+
+    public static long moveStandardToItemFluid(FluidContainer from, PlatformFluidItemHandler to, ItemStackHolder receiver, long maxInsert, boolean simulate) {
+        for (int i = 0; i < from.getSize(); i++) {
+            FluidHolder fluid = from.getFluids().get(i);
+            FluidHolder extracted = from.extractFluid(fluid, true);
+            if (!extracted.isEmpty()){
+                long inserted = to.insertFluid(receiver.copy(), extracted, true);
+                if (inserted > 0){
+                    FluidHolder fluidHolder = FluidHooks.newFluidHolder(fluid.getFluid(), Math.min(maxInsert, inserted), fluid.getCompound());
+                    from.extractFluid(fluidHolder, simulate);
+                    return to.insertFluid(receiver, fluidHolder, simulate);
+                }
+
+            }
+        }
+        return 0;
+    }
+
+    public static long moveItemToStandardFluid(PlatformFluidItemHandler from, FluidContainer to, ItemStackHolder receiver, long maxExtract, boolean simulate) {
+        for (int i = 0; i < from.getTankAmount(); i++) {
+            FluidHolder fluid = from.getFluidInTank(i);
+            FluidHolder extracted = from.extractFluid(receiver.copy(), fluid, true);
+            if (!extracted.isEmpty()){
+                long inserted = to.insertFluid(extracted, true);
+                if (inserted > 0){
+                    FluidHolder fluidHolder = FluidHooks.newFluidHolder(fluid.getFluid(), Math.min(maxExtract, inserted), fluid.getCompound());
+                    from.extractFluid(receiver, fluidHolder, simulate);
+                    return to.insertFluid(fluidHolder, simulate);
+                }
+
+            }
+        }
+        return 0;
+    }
 
     public static void writeToPacket(FriendlyByteBuf buffer, FluidHolder holder) {
         if (holder.isEmpty()) {
