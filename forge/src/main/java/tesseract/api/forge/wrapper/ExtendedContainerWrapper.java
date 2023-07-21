210491dadda2ec055e4d250dd73fa6635dfa98e6
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-07-21 17:02:35 -0400

    added ExtendedContainerWrapper

---
+++ forge/src/main/java/tesseract/api/forge/wrapper/ExtendedContainerWrapper.java
@@ -0,0 +1,25 @@
+package tesseract.api.forge.wrapper;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.items.wrapper.InvWrapper;
+import org.jetbrains.annotations.NotNull;
+import tesseract.api.item.ExtendedItemContainer;
+
+public class ExtendedContainerWrapper extends InvWrapper {
+    public ExtendedContainerWrapper(ExtendedItemContainer inv) {
+        super(inv);
+    }
+
+    @NotNull
+    @Override
+    public ItemStack insertItem(int slot, @NotNull ItemStack stack, boolean simulate) {
+        return ((ExtendedItemContainer)getInv()).insertItem(slot, stack, simulate);
+    }
+
+    @NotNull
+    @Override
+    public ItemStack extractItem(int slot, int amount, boolean simulate) {
+        return ((ExtendedItemContainer)getInv()).extractItem(slot, amount, simulate);
+    }
+}
