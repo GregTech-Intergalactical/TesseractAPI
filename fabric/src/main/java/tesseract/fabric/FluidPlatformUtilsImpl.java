ed7385ab510e03d3cf21bcc3ca7d24a2d7eeab44
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-07-21 21:09:46 -0400

    removed porting lib dep

--- fabric/src/main/java/tesseract/fabric/FluidPlatformUtilsImpl.java
+++ fabric/src/main/java/tesseract/fabric/FluidPlatformUtilsImpl.java
@@ -1,9 +1,13 @@
 package tesseract.fabric;
 
 import earth.terrarium.botarium.common.fluid.base.FluidHolder;
-import earth.terrarium.botarium.fabric.fluid.holder.FabricFluidHolder;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
+import net.fabricmc.fabric.api.transfer.v1.client.fluid.FluidVariantRendering;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariantAttributes;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -13,11 +17,23 @@ import net.minecraft.world.level.material.Fluid;
 public class FluidPlatformUtilsImpl {
 
     public static ResourceLocation getStillTexture(Fluid fluid){
-        return fluid.getAttributes().getStillTexture();
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            FluidVariant variant = FluidVariant.of(fluid);
+            TextureAtlasSprite[] sprites = FluidVariantRendering.getSprites(variant);
+
+            return sprites[0] == null ? SimpleFluidRenderHandler.WATER_STILL : sprites[0].getName();
+        }
+        return new ResourceLocation("block/water_still");
     }
 
     public static ResourceLocation getFlowingTexture(Fluid fluid){
-        return fluid.getAttributes().getFlowingTexture();
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            FluidVariant variant = FluidVariant.of(fluid);
+            TextureAtlasSprite[] sprites = FluidVariantRendering.getSprites(variant);
+
+            return sprites[1] == null ? SimpleFluidRenderHandler.WATER_STILL : sprites[1].getName();
+        }
+        return new ResourceLocation("block/water_still");
     }
     public static ResourceLocation getFluidId(Fluid fluid){
         return Registry.FLUID.getKey(fluid);
@@ -32,7 +48,10 @@ public class FluidPlatformUtilsImpl {
     }
 
     public static int getFluidColor(Fluid fluid){
-        return fluid.getAttributes().getColor();
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            return FluidVariantRendering.getColor(FluidVariant.of(fluid));
+        }
+        return -1;
     }
 
     public static SoundEvent getFluidSound(Fluid fluid, boolean fill){
