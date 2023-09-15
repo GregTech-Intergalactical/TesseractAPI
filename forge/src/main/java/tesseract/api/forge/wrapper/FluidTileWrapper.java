601e0771c05de86e045f779fcc018bedc06b37ef
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-09-15 14:22:05 -0400

    switched more javax annotations to jetbrains

--- forge/src/main/java/tesseract/api/forge/wrapper/FluidTileWrapper.java
+++ forge/src/main/java/tesseract/api/forge/wrapper/FluidTileWrapper.java
@@ -8,9 +8,9 @@ import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.fluids.capability.IFluidHandler;
+import org.jetbrains.annotations.NotNull;
 import tesseract.api.fluid.IFluidNode;
 
-import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -67,7 +67,7 @@ public record FluidTileWrapper(BlockEntity tile,
         return null;
     }
 
-    @Nonnull
+    @NotNull
     @Override
     public FluidHolder getFluidInTank(int tank) {
         return new ForgeFluidHolder(handler.getFluidInTank(tank));
@@ -98,7 +98,7 @@ public record FluidTileWrapper(BlockEntity tile,
     }
 
     @Override
-    public boolean isFluidValid(int tank, @Nonnull FluidHolder stack) {
+    public boolean isFluidValid(int tank, @NotNull FluidHolder stack) {
         return handler.isFluidValid(tank, new ForgeFluidHolder(stack).getFluidStack());
     }
 
