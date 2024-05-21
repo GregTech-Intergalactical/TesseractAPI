e87b4d4f806e52948125a3c25571623b89c0c779
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-05-20 22:35:57 -0400

    started work on uodating botarium

--- common/src/main/java/tesseract/FluidPlatformUtils.java
+++ common/src/main/java/tesseract/FluidPlatformUtils.java
@@ -3,7 +3,10 @@ package tesseract;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 
 
+import earth.terrarium.botarium.common.fluid.FluidApi;
+import earth.terrarium.botarium.common.fluid.base.FluidContainer;
 import earth.terrarium.botarium.common.fluid.base.FluidHolder;
+import earth.terrarium.botarium.common.fluid.base.ItemFluidContainer;
 import earth.terrarium.botarium.common.fluid.base.PlatformFluidHandler;
 import earth.terrarium.botarium.common.fluid.base.PlatformFluidItemHandler;
 import earth.terrarium.botarium.common.fluid.utils.FluidHooks;
@@ -26,7 +29,7 @@ public abstract class FluidPlatformUtils {
     //public static FluidPlatformUtils INSTANCE = new FluidPlatformUtils();
 
     public static FluidHolder createFluidStack(Fluid fluid, long amount){
-        return FluidHooks.newFluidHolder(fluid,amount, null);
+        return FluidHolder.of(fluid,amount);
     }
 
     public abstract ResourceLocation getStillTexture(Fluid fluid);
@@ -49,7 +52,7 @@ public abstract class FluidPlatformUtils {
 
     /**
      * Fill a destination fluid handler from a source fluid handler with a max amount.
-     * To specify a fluid to transfer instead of max amount, use {@link #tryFluidTransfer(PlatformFluidHandler, PlatformFluidHandler, FluidHolder, boolean)}
+     * To specify a fluid to transfer instead of max amount, use {@link #tryFluidTransfer(FluidContainer, FluidContainer, FluidHolder, boolean)}
      * To transfer as much as possible, use {@link Integer#MAX_VALUE} for maxAmount.
      *
      * @param fluidDestination The fluid handler to be filled.
@@ -59,18 +62,18 @@ public abstract class FluidPlatformUtils {
      * @return the fluidStack that was transferred from the source to the destination. null on failure.
      */
     @NotNull
-    public FluidHolder tryFluidTransfer(PlatformFluidHandler fluidDestination, PlatformFluidHandler fluidSource, long maxAmount, boolean doTransfer) {
-        for (int i = 0; i < fluidSource.getTankAmount(); i++) {
-            FluidHolder fluid = fluidSource.getFluidInTank(i);
+    public FluidHolder tryFluidTransfer(FluidContainer fluidDestination, FluidContainer fluidSource, long maxAmount, boolean doTransfer) {
+        for (int i = 0; i < fluidSource.getSize(); i++) {
+            FluidHolder fluid = fluidSource.getFluids().get(i);
             FluidHolder transfer = tryFluidTransfer(fluidDestination, fluidSource, fluid.copyWithAmount(Math.min(fluid.getFluidAmount(), maxAmount)), doTransfer);
             if (!transfer.isEmpty()) return transfer;
         }
-        return FluidHooks.emptyFluid();
+        return FluidHolder.empty();
     }
 
     /**
      * Fill a destination fluid handler from a source fluid handler using a specific fluid.
-     * To specify a max amount to transfer instead of specific fluid, use {@link #tryFluidTransfer(PlatformFluidHandler, PlatformFluidHandler, long, boolean)}
+     * To specify a max amount to transfer instead of specific fluid, use {@link #tryFluidTransfer(FluidContainer, FluidContainer, long, boolean)}
      * To transfer as much as possible, use {@link Integer#MAX_VALUE} for resource.amount.
      *
      * @param fluidDestination The fluid handler to be filled.
@@ -80,25 +83,25 @@ public abstract class FluidPlatformUtils {
      * @return the FluidHolder that was transferred from the source to the destination. null on failure.
      */
     @NotNull
-    public FluidHolder tryFluidTransfer(PlatformFluidHandler fluidDestination, PlatformFluidHandler fluidSource, FluidHolder resource, boolean doTransfer)
+    public FluidHolder tryFluidTransfer(FluidContainer fluidDestination, FluidContainer fluidSource, FluidHolder resource, boolean doTransfer)
     {
         FluidHolder drainable = fluidSource.extractFluid(resource, true);
         if (!drainable.isEmpty() && resource.matches(drainable))
         {
             return tryFluidTransfer_Internal(fluidDestination, fluidSource, drainable, doTransfer);
         }
-        return FluidHooks.emptyFluid();
+        return FluidHolder.empty();
     }
 
     /**
      * Internal method for filling a destination fluid handler from a source fluid handler using a specific fluid.
      * Assumes that "drainable" can be drained from "fluidSource".
      *
-     * Modders: Instead of this method, use {@link #tryFluidTransfer(PlatformFluidHandler, PlatformFluidHandler, FluidHolder, boolean)}
-     * or {@link #tryFluidTransfer(PlatformFluidHandler, PlatformFluidHandler, long, boolean)}.
+     * Modders: Instead of this method, use {@link #tryFluidTransfer(FluidContainer, FluidContainer, FluidHolder, boolean)}
+     * or {@link #tryFluidTransfer(FluidContainer, FluidContainer, long, boolean)}.
      */
     @NotNull
-    private static FluidHolder tryFluidTransfer_Internal(PlatformFluidHandler fluidDestination, PlatformFluidHandler fluidSource, FluidHolder drainable, boolean doTransfer)
+    private static FluidHolder tryFluidTransfer_Internal(FluidContainer fluidDestination, FluidContainer fluidSource, FluidHolder drainable, boolean doTransfer)
     {
         long fillableAmount = fluidDestination.insertFluid(drainable, true);
         if (fillableAmount > 0)
@@ -118,31 +121,32 @@ public abstract class FluidPlatformUtils {
                 return drainable;
             }
         }
-        return FluidHooks.emptyFluid();
+        return FluidHolder.empty();
     }
 
-    public boolean fillItemFromContainer(ItemStack stack, PlatformFluidHandler handler, Consumer<ItemStack> consumer){
+    public boolean fillItemFromContainer(ItemStack stack, FluidContainer handler, Consumer<ItemStack> consumer){
         return fillItemFromContainer(stack, handler, s -> true, consumer);
     }
 
-    public boolean emptyItemIntoContainer(ItemStack stack, PlatformFluidHandler handler, Consumer<ItemStack> consumer){
+    public boolean emptyItemIntoContainer(ItemStack stack, FluidContainer handler, Consumer<ItemStack> consumer){
         return emptyItemIntoContainer(stack, handler, s -> true, consumer);
     }
 
-    public boolean fillItemFromContainer(ItemStack stack, PlatformFluidHandler handler, Predicate<ItemStack> tester, Consumer<ItemStack> consumer){
-        PlatformFluidItemHandler itemHandler = FluidHooks.safeGetItemFluidManager(stack.copy()).orElse(null);
+    public boolean fillItemFromContainer(ItemStack stack, FluidContainer handler, Predicate<ItemStack> tester, Consumer<ItemStack> consumer){
+        ItemStackHolder holder = new ItemStackHolder(stack);
+        ItemFluidContainer itemHandler = FluidContainer.of(holder);
         if (itemHandler == null) return false;
-        for (int i = 0; i < handler.getTankAmount(); i++) {
-            FluidHolder fluid = handler.getFluidInTank(i);
+        for (int i = 0; i < handler.getSize(); i++) {
+            FluidHolder fluid = handler.getFluids().get(i);
             FluidHolder extracted = handler.extractFluid(fluid, true);
             if (!extracted.isEmpty()){
-                ItemStackHolder holder = new ItemStackHolder(stack);
-                long inserted = itemHandler.insertFluid(holder, extracted, false);
-                if (inserted > 0 && tester.test(holder.getStack())){
-                    itemHandler = FluidHooks.getItemFluidManager(stack);
-                    FluidHolder fluidHolder = FluidHooks.newFluidHolder(fluid.getFluid(), inserted, fluid.getCompound());
+                ItemStackHolder holder2 = new ItemStackHolder(stack.copy());
+                ItemFluidContainer test = FluidContainer.of(holder2);
+                long inserted = test.insertFluid(extracted, false);
+                if (inserted > 0 && tester.test(holder2.getStack())){
+                    FluidHolder fluidHolder = FluidHolder.of(fluid.getFluid(), inserted, fluid.getCompound());
                     handler.extractFluid(fluidHolder, false);
-                    long insert = itemHandler.insertFluid(holder, fluidHolder, false);
+                    long insert = itemHandler.insertFluid(fluidHolder, false);
                     consumer.accept(holder.getStack());
                     return insert > 0;
                 }
@@ -151,19 +155,20 @@ public abstract class FluidPlatformUtils {
         return false;
     }
 
-    public boolean emptyItemIntoContainer(ItemStack stack, PlatformFluidHandler handler, Predicate<ItemStack> tester, Consumer<ItemStack> consumer){
-        PlatformFluidItemHandler itemHandler = FluidHooks.safeGetItemFluidManager(stack.copy()).orElse(null);
+    public boolean emptyItemIntoContainer(ItemStack stack, FluidContainer handler, Predicate<ItemStack> tester, Consumer<ItemStack> consumer){
+        ItemStackHolder holder = new ItemStackHolder(stack);
+        ItemFluidContainer itemHandler = FluidContainer.of(holder);
         if (itemHandler == null) return false;
-        for (int i = 0; i < itemHandler.getTankAmount(); i++) {
-            FluidHolder fluid = itemHandler.getFluidInTank(i);
-            ItemStackHolder holder = new ItemStackHolder(stack);
-            FluidHolder extracted = itemHandler.extractFluid(holder, fluid, false);
-            if (!extracted.isEmpty() && tester.test(holder.getStack())){
+        for (int i = 0; i < itemHandler.getSize(); i++) {
+            FluidHolder fluid = itemHandler.getFluids().get(i);
+            ItemStackHolder holder2 = new ItemStackHolder(stack.copy());
+            ItemFluidContainer test = FluidContainer.of(holder2);
+            FluidHolder extracted = test.extractFluid(fluid, false);
+            if (!extracted.isEmpty() && tester.test(holder2.getStack())){
                 long inserted = handler.insertFluid(extracted, true);
                 if (inserted > 0){
-                    itemHandler = FluidHooks.getItemFluidManager(stack.copy());
-                    FluidHolder fluidHolder = FluidHooks.newFluidHolder(fluid.getFluid(), inserted, fluid.getCompound());
-                    long actuallyExtracted = itemHandler.extractFluid(holder, fluidHolder, false).getFluidAmount();
+                    FluidHolder fluidHolder = FluidHolder.of(fluid.getFluid(), inserted, fluid.getCompound());
+                    long actuallyExtracted = itemHandler.extractFluid(fluidHolder, false).getFluidAmount();
                     if (actuallyExtracted == inserted){
                         long insert = handler.insertFluid(fluidHolder, false);
                         consumer.accept(holder.getStack());
